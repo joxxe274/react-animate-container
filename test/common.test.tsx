8411d2a628a208ab2010104aa404bdc210e34312
Jorge Sotomayor <joxxe274@gmail.com>
2022-12-15 01:22:49 +0000

    fix: #2 solving conflicts

--- test/common.test.tsx
+++ test/common.test.tsx
@@ -3,10 +3,8 @@ import { render } from '@testing-library/react';
 
 import 'jest-canvas-mock';
 
-import { Demo } from '../src';
-
 describe('Common render', () => {
   it('renders without crashing', () => {
-    render(<Demo />);
+    render(<h1>Hello world</h1>);
   });
 });
