e5ad61f55be3240d1d58458977df9eeea7e18129
Jorge Sotomayor <joxxe274@gmail.com>
2022-12-15 01:17:08 +0000

    fix: #2 solving conflicts

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,29 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-=======
-import { Demo } from 'react-animate-container';
->>>>>>> main
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <div>
-<<<<<<< HEAD
       <h2>react-animate-container</h2>
     </div>
     <hr />
     <div></div>
-=======
-      <h2>Default Demo</h2>
-      <Demo />
-    </div>
-    <hr />
-    <div>
-      <h2>Demo with predefined greeting</h2>
-      <Demo greeting='Joxxe274' />
-    </div>
->>>>>>> main
   </React.StrictMode>,
 );
